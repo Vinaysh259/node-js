91acbcd748eac1271ad2cb3c65383eeda8bbecd2
Vinay Hegde <vinaysh259@gmail.com>
2020-09-09 22:19:51 +0530

    Updated app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,21 @@
-console.log('Starting')
+// console.log('Starting')
 
-setTimeout( () => {
-    console.log('2 seconds delay')
-},2000)
+// setTimeout( () => {
+//     console.log('2 seconds delay')
+// },2000)
 
-setTimeout( () => {
-    console.log('0 sec delay')
-}, 0)
+// setTimeout( () => {
+//     console.log('0 sec delay')
+// }, 0)
 
-console.log('Stopping')
+// console.log('Stopping')
+
+const request = require('postman-request')
+
+const url = 'http://api.weatherstack.com/current?access_key=4a680f9000818bc494865ab0547e07ad&query=40.7127,-74.0059'
+
+request({uri: url}, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
+
